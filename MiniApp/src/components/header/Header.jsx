eb63f96636d932feda439c202dbb95e1496d1edf
DimaSt2 <svoy2@tut.by>
2025-06-08 11:57:39 +0300

    add: components Header, PlayerInfo

---
+++ MiniApp/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import Spinner from '../spinner/Spinner';
+import './header.css';
+
+
+const Header = () => {
+    return (
+        <>
+        <h1 className='h1'>Poker</h1>
+        <Spinner />
+           
+        </>
+    );
+};
+
+export default Header;
